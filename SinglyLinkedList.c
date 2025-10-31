968ebde8d1e572306756d68a5073738460a30419
Christo Martin <christomartincombat@gmail.com>
2025-10-31 22:23:22 +0530

    Declare head and temp pointers after structure definition.
    Changed the order of declarations for head and temp pointers to after the structure definition.

--- SinglyLinkedList.c
+++ SinglyLinkedList.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node* head=NULL;
-struct node* temp=NULL;
-
 struct node{
     int data;
     struct node* link;
 };
+
+struct node* head=NULL;
+struct node* temp=NULL;
+
 
 //insertion at beginning of sll(singly linked list)
 void insert_beg()
